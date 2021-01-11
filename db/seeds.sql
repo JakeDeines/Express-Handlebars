6e38e51cdeb0350731189a5280008500469851a6
JakeDeines <67669417+JakeDeines@users.noreply.github.com>
2021-01-10 18:05:02 -0800

    Update seeds.sql
    updated values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 USE burgers_db;
 INSERT INTO burgers(burger_name, devoured)
-VALUES ("hamburger",false), ("cheeseburger",false), ("veggieburger",false);
+VALUES ("KrabbyPatty",false)"Jelly Patty",false)"King-Size Ultra Krabby Supreme",false)"Giant Krabby Patty",false);
